cad9a9fa92738ac1bdb5127bd7622e2b237a0b3f
Abdulkadir Nursumar <a.k.nursumar@gmail.com>
2026-07-29 10:59:24 +0530

    Time: 0 ms (100%), Space: 19.3 MB (58.22%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -1,9 +1,16 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
+        # arr = []
+        # while(n>0):
+        #     arr.append(n%10)
+        #     n = n//10
+        # arr.sort()
+        # return arr[len(arr)-1]*arr[len(arr)-2]
+        s1 = str(n)
         arr = []
-        while(n>0):
-            arr.append(n%10)
-            n = n//10
+        for ch in s1:
+            arr.append(int(ch))
         arr.sort()
         return arr[len(arr)-1]*arr[len(arr)-2]
+
         
