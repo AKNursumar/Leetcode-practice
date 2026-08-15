d421c3b9d3b2d3527939d7a384813d1e827529a8
Abdulkadir Nursumar <a.k.nursumar@gmail.com>
2026-08-15 17:19:17 +0530

    Time: 415 ms (5%), Space: 20.4 MB (93.58%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -3,7 +3,7 @@ class Solution:
         s1 = ""
         for ch in s:
             if ch == '*':
-                s1 = s1[0:len(s1)-1]
+                s1 = s1[0:-1]
             else:
                 s1 = s1 + ch
         return s1
