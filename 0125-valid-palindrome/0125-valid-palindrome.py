83c1a36fccd4788d78c0b645c711e4129d27951f
Abdulkadir Nursumar <a.k.nursumar@gmail.com>
2026-08-17 14:11:26 +0530

    Time: 11 ms (31.33%), Space: 19.8 MB (33.81%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1 = ""
+        for ch in s:
+            if ch.isalnum():
+                s1 = s1+ch.lower()
+        left = 0
+        right = len(s1)-1
+        while left<=right:
+            if s1[left]==s1[right]:
+                left+=1
+                right-=1
+            else:
+                return False
+        return True
+        
+        
