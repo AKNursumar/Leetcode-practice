bafe38e6ae0fd680253a46e2f8d1fc91c730cdd6
Abdulkadir Nursumar <a.k.nursumar@gmail.com>
2026-09-20 12:52:10 +0530

    Time: 3 ms (97.65%), Space: 19.3 MB (55.57%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,7 +1,9 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        s1 = "0zyxwvutsrqponmlkjihgfedcba"
+        dic = {
+            'a':26,'b':25,'c':24,'d':23,'e':22,'f':21,'g':20,'h':19,'i':18,'j':17,'k':16,'l':15,'m':14,'n':13,'o':12,'p':11,'q':10,'r':9,'s':8,'t':7,'u':6,'v':5,'w':4,'x':3,'y':2,'z':1
+        }
         su = 0
         for i in range(len(s)):
-            su += s1.index(s[i])*(i+1)
+            su += dic[s[i]]*(i+1)
         return su
